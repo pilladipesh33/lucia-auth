dfacbeeec41f97a4e4e67f07b06840d767cf1e9c
Pilla Dipesh <pilladipesh.pd@gmail.com>
2024-08-02 23:26:15 +0530

    inital commit

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthenticationPage = () => {
+  return (
+    <div className='relative bg-background flex h-screen w-full justify-center items-center'>
+      <div className='max-w-3xl'>hi</div>
+    </div>
+  )
+}
+
+export default AuthenticationPage
